d68ab6ce1008f0f6b047c3886651f877826bed3a
Subhashinibalu <subhashiniulk@gmail.com>
2024-05-04 22:39:35 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -27,9 +27,7 @@ const Navbar = ({value}) => {
                     </ul>
                     <form className="d-flex">
                         <button className="btn btn-outline-dark" type="submit">
-                            <i className="bi-cart-fill me-1"></i>
-                            Cart
-                            <span className="badge bg-dark text-white ms-1 rounded-pill"> {value}</span>
+                            <i className="bi-cart-fill me-1"></i> Cart <span className="badge bg-dark text-white ms-1 rounded-pill"> {value}</span>
                         </button>
                     </form>
                 </div>
