95fa5280280d09ee00a6bbb999fab79b7b889fcc
Subhashinibalu <subhashiniulk@gmail.com>
2024-05-04 22:32:19 +0530

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
        <>
        <footer className="py-5 bg-dark">
-            <div className="container"><p className="m-0 text-center text-white">Copyright © Your Website 2023</p></div>
+            <div className="container"><p className="m-0 text-center text-white">Copyright © Your Website 2024</p></div>
         </footer>
        </>
     );
